74ae47a729d978acc4126a7b2a78eeec1098b9cc
ayazabbas <lfcayaz@gmail.com>
2026-05-10 21:18:25 +0000

    Use deployer key for daily check-in deploy script

--- contracts/script/DeployDailyCheckIn.s.sol
+++ contracts/script/DeployDailyCheckIn.s.sol
@@ -6,8 +6,16 @@ import "../src/DailyCheckIn.sol";
 
 contract DeployDailyCheckInScript is Script {
     function run() external returns (DailyCheckIn checkIn) {
-        vm.startBroadcast();
+        uint256 pk = _privateKey();
+        vm.startBroadcast(pk);
         checkIn = new DailyCheckIn();
         vm.stopBroadcast();
+    }
+
+    function _privateKey() internal view returns (uint256) {
+        uint256 fallbackKey = vm.envOr("DEPLOYER_PRIVATE_KEY", uint256(0));
+        uint256 pk = vm.envOr("PRIVATE_KEY", fallbackKey);
+        require(pk != 0, "DeployDailyCheckIn: missing private key");
+        return pk;
     }
 }
